7ee7e8009f67a930bf0d3e480d5d992b55769ff7
Sanyam Ratreja <sanyamratreja18@gmail.com>
2024-05-19 20:41:32 +0530

    updated resume

--- constants/links.ts
+++ constants/links.ts
@@ -1,4 +1,4 @@
-export const resumeLink = "https://drive.google.com/file/d/11FcPH0lFJ6rhFXPQwCGzrsaZ3VNLzAgr/view?usp=sharing";
+export const resumeLink = "https://drive.google.com/file/d/1gHz1GvgACG4h00xmsIvPTpLQsCO1siLy/view?usp=sharing";
 export const linkedInLink = "https://www.linkedin.com/in/sanyam-ratreja/";
 export const twitterLink = "https://twitter.com/SanyamRatr61964";
 export const githubLink = "https://github.com/sanyam12";
